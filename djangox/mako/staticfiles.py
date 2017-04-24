9a553b9737c1ee1b84597344640a7a024b14b281
Pak Youngrok <pak.youngrok@gmail.com>
2017-04-24 13:15:54 +0900

    fix path not found case

--- djangox/mako/staticfiles.py
+++ djangox/mako/staticfiles.py
@@ -1,17 +1,22 @@
 import hashlib
 
 import os
+from urllib.parse import urljoin
+
 from django.core.cache import cache
-from django.templatetags.static import StaticNode
+from django.templatetags.static import StaticNode, PrefixNode
 from django.contrib.staticfiles import finders
 
 
 def static(path):
+    prefixed_path = urljoin(PrefixNode.handle_simple("STATIC_URL"), path)
+
     if not cache.get('statichash.' + path):
         fs_path = finders.find(path)
-        if not os.path.isfile(fs_path): return StaticNode.handle_simple(path)
+        if not fs_path or not os.path.isfile(fs_path):
+            return prefixed_path
 
         hash = hashlib.md5(open(fs_path, 'rb').read()).hexdigest()
         cache.set('statichash.' + path, hash)
 
-    return StaticNode.handle_simple(path) + '?' + cache.get('statichash.' + path)
+    return prefixed_path + '?' + cache.get('statichash.' + path)
