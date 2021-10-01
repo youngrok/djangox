ae47b381ed72510f63bc485dbc58a5962cc1499f
JunKi-Yoon <na86421@mailplug.co.kr>
2021-10-01 11:00:55 +0900

    url to re_path & getargspec to getfullargspec

--- djangox/route/__init__.py
+++ djangox/route/__init__.py
@@ -1,9 +1,11 @@
-from django.conf.urls import include, url
+from django.urls import re_path
+from django.conf.urls import include
 import inspect
 import pkgutil
 import re
 import sys
 import types
+
 
 def discover_controllers(package, method_first=False):
     '''
@@ -11,39 +13,44 @@ def discover_controllers(package, method_first=False):
     '''
     if isinstance(package, types.ModuleType):
         package = package.__name__
-        
+
     urls = []
     __import__(package)
 
     if hasattr(sys.modules[package], 'index'):
-        urls.append(url('^$', getattr(sys.modules[package], 'index')))
-    
+        urls.append(re_path('^$', getattr(sys.modules[package], 'index')))
+
     for _, name, _ in pkgutil.iter_modules([sys.modules[package].__path__[0]]):
         __import__(package + '.' + name)
-        
+
         controller = sys.modules[package + '.' + name]
-        
+
         for member in dir(controller):
             func = getattr(controller, member)
-            if not inspect.isfunction(func): continue
-            args = inspect.getargspec(func).args
-            
+            if not inspect.isfunction(func):
+                continue
+            inspect.getfullargspec(func).args
+
             # TODO filter request functions
             # if len(args) == 0 or args[0] != 'request': continue
 
             if method_first:
-                urls.append(url('^' + name + '/' + member + '/(?P<resource_id>[^/\?\&]+)/?$', func))
+                urls.append(re_path('^' + name + '/' + member + '/(?P<resource_id>[^/\?\&]+)/?$',
+                            func))
             else:
-                urls.append(url('^' + name + '/(?P<resource_id>[^/\?\&]+)/' + member + '/?$', func))
+                urls.append(re_path('^' + name + '/(?P<resource_id>[^/\?\&]+)/' + member + '/?$',
+                            func))
 
-            urls.append(url('^' + name + '/' + member + '/?$', func))
+            urls.append(re_path('^' + name + '/' + member + '/?$', func))
 
         if 'show' in dir(controller):
-            urls.append(url('^' + name + '/(?P<resource_id>[^/\?\&]+)/?$', getattr(controller, 'show')))
+            urls.append(re_path('^' + name + '/(?P<resource_id>[^/\?\&]+)/?$',
+                        getattr(controller, 'show')))
         if 'index' in dir(controller):
-            urls.append(url('^' + name + '$', getattr(controller, 'index')))
-                
+            urls.append(re_path('^' + name + '$', getattr(controller, 'index')))
+
     return include(urls)
+
 
 class SubdomainMiddleware(object):
     def process_request(self, request):
