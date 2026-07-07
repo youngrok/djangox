b8be29f55202e275c3f98a6a9049053057a5a246
Youngrok Pak <pak.youngrok@gmail.com>
2026-07-08 02:12:47 +0900

    Run pyinfra through current Python

--- djangox/deploy/control.py
+++ djangox/deploy/control.py
@@ -50,7 +50,8 @@ def infra_setup(environment: str):
 @app.command()
 def deploy(environment: str):
     Conf = load_conf(environment)
-    run(['pyinfra', '-y', f'deploy/{environment}.py', 'deploy/web.py'],
+    run([sys.executable, '-m', 'pyinfra', '-y',
+         f'deploy/{environment}.py', 'deploy/web.py'],
         env=command_env(Conf, environment))
 
 
